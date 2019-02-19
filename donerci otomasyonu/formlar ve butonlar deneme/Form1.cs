aa2141937b26ebf73ef0665505e9391ba1dd63bf
MusaKandemir <35560130+MusaKandemir@users.noreply.github.com>
2019-02-19 23:40:52 +0300

    Add files via upload

---
+++ donerci otomasyonu/formlar ve butonlar deneme/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace formlar_ve_butonlar_deneme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox3.Text==" ")
+            {
+                MessageBox.Show("Lütfen Adres Giriniz...");
+            }
+            
+            listBox1.Items.Add(textBox1.Text);
+            listBox2.Items.Add(textBox2.Text);
+            listBox3.Items.Add(textBox3.Text);
+            listBox4.Items.Add(comboBox1.Text);
+            listBox5.Items.Add(comboBox2.Text);
+            listBox6.Items.Add(comboBox3.Text);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            comboBox1.Text = " ";
+            comboBox2.Text = " ";
+            comboBox3.Text = " ";
+            
+                 
+        }
+    }
+}
